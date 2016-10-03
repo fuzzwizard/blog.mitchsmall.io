bcdf4758c04419e9130163c7a17e846b53605015
fuzzwizard <momojak@gmail.com>
2016-10-03 11:13:53 -0700

    Safety first.

---
+++ config.example.js
@@ -0,0 +1,20 @@
+exports.DEFINES = {
+  blog: {
+    // Metadata
+  },
+  widget: [
+    // Widget entries
+  ],
+  CDN: {
+    // Various cdns
+  }
+};
+
+exports.PORT = 8080;
+
+exports.PATHS = {
+  'source': 'src',
+  'destination': 'public'
+};
+
+exports.ADDRESS = '127.0.0.1';
